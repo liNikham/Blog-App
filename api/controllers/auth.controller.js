ae1d033adf0b036b8270716f75415f57dc378540
liNikham <mahadiknikhil2508@gmail.com>
2024-07-12 13:13:43 +0530

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,11 @@
 const User = require('../models/User.model')
 const bcryptjs = require('bcryptjs');
+const { errorHandler } = require('../utils/error');
+const jwt= require('jsonwebtoken')
 exports.signup= async(req,res,next)=>{
   const {username,email,password}= req.body;
   if(!username || !email || !password || username==='' || email === '' , password===''){
-     next(errorHandler(400,'All fields are required'));
+    return next(errorHandler(400,'All fields are required'));
   }
   const hashedPassword = bcryptjs.hashSync(password,10);
   const newUser = new User({
@@ -15,4 +17,31 @@ exports.signup= async(req,res,next)=>{
   }catch(err){
     next(err);
   }    
+}
+
+exports.signin = async( req,res,next)=>{
+    const {email,password}= req.body;
+    if(!email || !password || email==='' || password===''){
+             return next(errorHandler(400,'All fields are required'));
+    }
+    try{
+        const validUser = await User.findOne({email})
+        if(!validUser){
+           return  next(errorHandler(404,'Credentials Invalid'));
+        }
+        const validPassword = bcryptjs.compareSync(password,validUser.password);
+        if(!validPassword){
+           return next(errorHandler(404,'Credentials Invalid'))
+        }
+        const { password:pass,...rest} = validUser._doc;
+     const token = jwt.sign({
+        id:validUser._id,
+        
+     }, process.env.JWT_SECRET);
+     res.status(200).cookie('token',token,{
+        httpOnly:true
+     }).json(rest)
+    }catch(error){
+            next(error);
+    }
 }
--- api/routes/Auth.Routes.js
+++ api/routes/Auth.Routes.js
@@ -2,6 +2,7 @@ const express= require('express')
 const router = express.Router()
 const authController= require('../controllers/auth.controller')
 router.post('/signup',authController.signup);
+router.post('/signin',authController.signin);
 
 
 
