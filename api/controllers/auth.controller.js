0e8d40f4da5f81fa287434933b1243c402d114ce
liNikham <mahadiknikhil2508@gmail.com>
2024-07-12 10:48:26 +0530

    create signup UI

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,9 @@
 const User = require('../models/User.model')
 const bcryptjs = require('bcryptjs');
-exports.signup= async(req,res)=>{
+exports.signup= async(req,res,next)=>{
   const {username,email,password}= req.body;
   if(!username || !email || !password || username==='' || email === '' , password===''){
-    return res.status(400).json({message:'All fields are required'});
+     next(errorHandler(400,'All fields are required'));
   }
   const hashedPassword = bcryptjs.hashSync(password,10);
   const newUser = new User({
@@ -13,6 +13,6 @@ exports.signup= async(req,res)=>{
       await newUser.save();
       return res.status(200).json({message:"SignUp Successfull"});
   }catch(err){
-      return res.status(500).json({message:err.message})
+    next(err);
   }    
 }
