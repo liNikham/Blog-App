fe50da3ffc19f0967b89e7a955ed1c93173773e0
liNikham <mahadiknikhil2508@gmail.com>
2024-08-15 12:03:33 +0530

    Complete Dark mode Functionality

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -2,12 +2,15 @@ import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react'
 import React from 'react'
 import { Link,useLocation } from 'react-router-dom'
 import {AiOutlineSearch} from 'react-icons/ai'
-import {FaMoon} from 'react-icons/fa'
-import { useSelector } from 'react-redux'
+import {FaMoon,FaSun} from 'react-icons/fa'
+import { useDispatch, useSelector } from 'react-redux'
 import { RiZoomInLine } from 'react-icons/ri'
+import { toggleTheme } from '../redux/Theme/themeSlice'
 function Header() {
     const path=useLocation().pathname;
+    const dispatch = useDispatch();
   const {currentUser} = useSelector(state=>state.user)
+  const {theme}=useSelector(state=>state.theme);
   return (
    <Navbar className='border-b-2 mx-16 '>
          <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
@@ -21,8 +24,10 @@ function Header() {
             <AiOutlineSearch/>
          </Button>
          <div className='flex gap-2 md:order-2'>
-            <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
-                <FaMoon/>
+            <Button className='w-12 h-10 hidden sm:inline' color='gray' pill  onClick={()=>{
+              dispatch(toggleTheme());
+            }}>
+            {theme==='light'?<FaSun/>:<FaMoon/>}
             </Button>
             {
                 currentUser? (
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,9 +3,11 @@ import userReducer from './User/UserSlice'
 import persistReducer from 'redux-persist/es/persistReducer';
 import storage from 'redux-persist/lib/storage';
 import persistStore from 'redux-persist/es/persistStore';
+import themeReducer from './Theme/themeSlice'
 
 const rootReducer = combineReducers({
-    user:userReducer
+    user:userReducer,
+    theme:themeReducer
 
 });
 const persistConfig={
