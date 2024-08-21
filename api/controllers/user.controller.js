6c237f863885677d08b4e1dc0b16173ecd647149
liNikham <mahadiknikhil2508@gmail.com>
2024-08-21 20:11:25 +0530

    add user delete api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -48,4 +48,17 @@ const bcrypt = require('bcryptjs')
         }
       }
  
+exports.deleteUser = async(req,res,next)=>{
+  
+    if(req.user.id !== req.params.userId){
+        return next(errorHandler(403,"You are not allowed to delete this user"));
+    }
+    try{
+       await User.findByIdAndDelete(req.params.userId);
+       return res.status(200).json("User has been deleted");
+    }
+    catch(error){
+      next(error);
+    }
+}
 
--- api/routes/User.Routes.js
+++ api/routes/User.Routes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const {updateUser} = require('../controllers/user.controller.js');
 const {verifyUser} = require('../utils/verifyUser.js');
+const {deleteUser} = require('../controllers/user.controller.js');
 router.put('/update/:userId',verifyUser,updateUser);
-
+router.delete('/delete/:userId',verifyUser,deleteUser);
 
 module.exports=router;
