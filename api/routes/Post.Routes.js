0732509e4d110b658e4bfc3b69284a7bf142db03
liNikham <mahadiknikhil2508@gmail.com>
2024-08-31 23:20:02 +0530

    create getPosts api route functionality

--- api/routes/Post.Routes.js
+++ api/routes/Post.Routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const {createPost}= require('../controllers/post.controller');
+const {getPosts}= require('../controllers/post.controller');
 const {verifyUser}= require('../utils/verifyUser');
 router.post('/create',verifyUser,createPost);
+router.get('/getPosts',getPosts);
 
 
 
