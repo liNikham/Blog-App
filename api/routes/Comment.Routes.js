7c87dd15498e1da6ee74276fcbc62124055e86d3
liNikham <mahadiknikhil2508@gmail.com>
2024-09-30 23:32:12 +0530

    complete like functionality

--- api/routes/Comment.Routes.js
+++ api/routes/Comment.Routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {createComment,getPostComments}= require('../controllers/comment.controller');
+const {createComment,getPostComments,likeComment}= require('../controllers/comment.controller');
 const {verifyUser}= require('../utils/verifyUser');
 
 router.post('/create',verifyUser,createComment);
 router.get('/getPostComments/:postId',getPostComments);
+router.put('/likeComment/:commentId',verifyUser,likeComment);
 module.exports = router;
